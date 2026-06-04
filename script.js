3cc65e1846fd7ca6591e25c2161aac71e9cfbeb4
ti3ss <84694585+ti3ss@users.noreply.github.com>
2026-06-04 08:39:48 +0700

    Refactor and restore script.js functionality

--- script.js
+++ script.js
@@ -3,24 +3,7 @@ script.js
    LOADER
 ========================= */
 
-window.addEventListener("load", () => {
 
-const loader =
-document.getElementById("loader");
-
-setTimeout(() => {
-
-loader.style.opacity = "0";
-
-setTimeout(() => {
-
-loader.style.display = "none";
-
-}, 500);
-
-}, 1000);
-
-});
 
 /* =========================
    DATA KERANJANG
