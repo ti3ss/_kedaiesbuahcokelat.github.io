94a7776514aae594ace5963a25afb6649671f498
ti3ss <84694585+ti3ss@users.noreply.github.com>
2026-06-14 21:37:20 +0700

    Refactor cart management functions and styles

--- script.js
+++ script.js
@@ -562,3 +562,26 @@ counter.innerHTML =
 "🛒 " + totalQty;
 
 }
+function filterMenu(category){
+
+const cards =
+document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+if(
+category === "all" ||
+card.classList.contains(category)
+){
+
+card.style.display = "block";
+
+}else{
+
+card.style.display = "none";
+
+}
+
+});
+
+}
