159dfe65aba836523cfebf48c5f56b8e24c5d840
ti3ss <84694585+ti3ss@users.noreply.github.com>
2026-06-16 21:34:07 +0700

    Refactor script.js to add dark mode functionality

--- script.js
+++ script.js
@@ -2,7 +2,34 @@
 /* =========================
    LOADER
 ========================= */
+const darkBtn =
+document.getElementById("darkModeBtn");
 
+darkBtn.addEventListener("click",()=>{
+
+document.body.classList.toggle(
+"dark-mode"
+);
+
+localStorage.setItem(
+"darkMode",
+document.body.classList.contains(
+"dark-mode"
+)
+);
+
+});
+
+if(
+localStorage.getItem("darkMode")
+=== "true"
+){
+
+document.body.classList.add(
+"dark-mode"
+);
+
+}
 
 
 /* =========================
